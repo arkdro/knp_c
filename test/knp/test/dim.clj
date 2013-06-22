a8f31ed94383bbc503fe6ab38cdb66ac75fe3b86
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-22 22:48:44 +0400

    add tests

--- test/knp/test/dim.clj
+++ test/knp/test/dim.clj
@@ -10,3 +10,30 @@
         ]
     (is (= act 1))))
 
+(deftest set-point-test
+  (let [h 3
+        w 2
+        x 2
+        y 3
+        val 24
+        table (vec (take (* w h) (repeat 1)))
+        act (set-point x y h val table)
+        exp [1 1 1 1 1 24]
+        ]
+    (is (= exp act))))
+
+(deftest get-prev-total-vals-test
+  (let [cur-c 3
+        c 3
+        x 3
+        wei 1
+        table [1 1 1 ;; this table looks transposed (x, y axis changed)
+               1 5 24
+               0 0 0]
+        act (get-prev-total-vals cur-c c x wei table)
+        exp [5 24]
+        ]
+    (is (= exp act)))
+  )
+
+
