fb6575db723e3fef27fd53ccd6cc0023f7cc9606
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-23 14:00:05 +0400

    add test for get-new-item

--- test/knp/test/backtrack.clj
+++ test/knp/test/backtrack.clj
@@ -26,3 +26,20 @@
         ]
     (is (= exp act))))
 
+(deftest get-new-item-test
+  (let [cur-c 6
+        c 7
+        item-idx 3
+        items [[16 2] [19 3] [23 4] [28 5]]
+        acc [0 0 0 1]
+        table [
+               0 16 16 16 16 16 16
+               0 16 19 19 35 35 35
+               0 16 19 23 35 39 42
+               0 16 19 23 35 39 44
+               ]
+        act (get-new-item cur-c c item-idx items acc table)
+        exp [2 2 [0 0 1 1]]
+        ]
+    (is (= exp act))))
+
