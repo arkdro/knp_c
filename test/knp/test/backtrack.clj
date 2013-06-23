97f785c46d838721f89fe18ba3c0fe8ff899017b
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-23 13:51:00 +0400

    add test for use-item

--- test/knp/test/backtrack.clj
+++ test/knp/test/backtrack.clj
@@ -13,3 +13,16 @@
         ]
     (is (= exp act))))
 
+(deftest use-item-test
+  (let [cur-c 4
+        c 5
+        item-idx 3
+        items [[16 2] [19 3] [23 4] [28 5]]
+        acc [0 0 0 1]
+        act (knp.backtrack/use-item cur-c c item-idx items acc)
+        exp [2
+             0
+             [0 0 1 1]]
+        ]
+    (is (= exp act))))
+
