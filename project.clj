0c3e4e6785d207a0a29ab5174aa78ea40aca82f8
Arkadiy Drovosekov <arkdro@gmail.com>
2013-06-23 23:13:31 +0400

    add profiler and memory to project

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject knp "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :jvm-opts ["-Xmx2g"]
+  :jvm-opts ["-Xmx2500m"]
   :main knp.core
   :dependencies [
     [org.clojure/clojure "1.3.0"]
     [org.clojure/tools.cli "0.2.2"]
+    [com.taoensso/timbre "2.1.2"]
     [org.clojure/tools.trace "0.7.5"]
   ])
